4a8728cfd4efcb3a63a294aa5e69b837a80c7182
yarkoslav <romanus004@gmail.com>
2020-12-17 16:34:18 +0200

    lol

--- weeks/week8/materials/hello_world.sh
+++ weeks/week8/materials/hello_world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # remember that on your device path will be another
-echo "Started. Date: $(date)" >> /home/pasha/Documents/Linux_club/UCU_Linux_Club/weeks/week8/materials/hello_world.log
+echo "Started. Date: $(date)" >> /home/yarmus/pr/clubs/UCU_Linux_Club/weeks/week8/materials
 while true
 do
 	echo "Hello, world"
